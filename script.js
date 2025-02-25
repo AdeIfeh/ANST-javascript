cc6354d45a07b1285e10927c425ca7296f70846d
AdeIfeh <iadebayoelizabeth@gmail.com>
2025-02-25 20:05:08 +0100

    Initial commit - Added JavaScript operations program

---
+++ script.js
@@ -0,0 +1,23 @@
+// Declare two variables and assign them any two numbers
+let num1 = 10;
+let num2 = 5;
+
+// Perform arithmetic operations and store the results in separate variables
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+// Print the results in a clear format
+console.log(`Numbers: num1 = ${num1}, num2 = ${num2}`);
+console.log(`Addition: ${num1} + ${num2} = ${addition}`);
+console.log(`Subtraction: ${num1} - ${num2} = ${subtraction}`);
+console.log(`Multiplication: ${num1} * ${num2} = ${multiplication}`);
+console.log(`Division: ${num1} / ${num2} = ${division}`);
+console.log(`Modulus: ${num1} % ${num2} = ${modulus}`);
+
+// Write output to a file
+//fs.writeFileSync('output.txt', output);
+
+//console.log('Results saved to output.txt');
