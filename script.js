209ee73cee7c5fd078132a25d9765ad0bb3d9334
AdeIfeh <iadebayoelizabeth@gmail.com>
2025-02-27 13:44:00 +0100

    Updated calculation output format

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ let division = num1 / num2;
 let modulus = num1 % num2;
 
 // Print the results in a clear format
-console.log(`Numbers: num1 = ${num1}, num2 = ${num2}`);
-console.log(`Addition: ${num1} + ${num2} = ${addition}`);
-console.log(`Subtraction: ${num1} - ${num2} = ${subtraction}`);
-console.log(`Multiplication: ${num1} * ${num2} = ${multiplication}`);
-console.log(`Division: ${num1} / ${num2} = ${division}`);
-console.log(`Modulus: ${num1} % ${num2} = ${modulus}`);
+console.log("Numbers: num1 = " + num1 + ", num2 = " + num2);
+console.log("Addition: " + num1 + " + " + num2 + " = " + addition);
+console.log("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
+console.log("Multiplication: " + num1 + " * " + num2 + " = " + multiplication);
+console.log("Division: " + num1 + " / " + num2 + " = " + division);
+console.log("Modulus: " + num1 + " % " + num2 + " = " + modulus);
 
 // Write output to a file
 //fs.writeFileSync('output.txt', output);
