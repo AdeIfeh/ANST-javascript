c5524c503702367894777b441b37a1a81fd8690e
AdeIfeh <iadebayoelizabeth@gmail.com>
2025-02-27 22:25:17 +0100

    ANST JavaScript arithmetic operations program

---
+++ script.js
@@ -0,0 +1,19 @@
+// Declare two variables and assign them any two numbers
+let num1 = 20;
+let num2 = 5;
+
+// Perform arithmetic operations and store the results in separate variables
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+/// Print the results
+console.log("Numbers =", "num1 =", num1, ", num2 =", num2);
+console.log("Addition =", addition);
+console.log("Subtraction =", subtraction);
+console.log("Multiplication =", multiplication);
+console.log("Division =", division);
+console.log("Modulus =", modulus);
+
